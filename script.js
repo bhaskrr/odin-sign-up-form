e7c204b6e280ffc5a4d25ed76a84b952a256a169
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2024-01-06 23:06:57 +0530

    Completed the JS file

---
+++ script.js
@@ -0,0 +1,26 @@
+// getting the submit button
+const submitBtn = document.querySelector('.sub-btn');
+
+const checkPasswordEquality = (e) =>{
+    e.preventDefault();
+
+    //getting values from DOM elements
+    const password = document.querySelector('#password');
+    const confirmedPassword = document.querySelector('#confirm-password');
+    const pwNotMatchText = document.querySelector('.pw-not-match');
+
+    // checking and manipulating
+    if(password.value == confirmedPassword.value){
+        password.classList.remove('red-border');
+        confirmedPassword.classList.remove('red-border');
+        pwNotMatchText.style.display = 'none';
+    }
+    else{
+        password.classList.add('red-border');
+        confirmedPassword.classList.add('red-border');
+        pwNotMatchText.style.display = 'block';
+    }
+}
+
+//calling the checker function when submit button is clicked
+submitBtn.addEventListener('click', checkPasswordEquality);
